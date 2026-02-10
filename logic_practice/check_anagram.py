684f5b94a8215f42f255cd00def2cec90859abca
RoshanPanthi11 <87538884+RoshanPanthi11@users.noreply.github.com>
2026-02-10 16:36:11 +0545

    Update check_anagram.py

--- logic_practice/check_anagram.py
+++ logic_practice/check_anagram.py
@@ -1,7 +1,7 @@
 str1 = input("Enter first string: ").replace(" ", "").lower()
 str2 = input("Enter second string: ").replace(" ", "").lower()
 
-# Method 1: Compare sorted strings
+
 if sorted(str1) == sorted(str2):
     print(f'"{str1}" and "{str2}" are anagrams.')
 else:
