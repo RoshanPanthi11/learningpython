e9ca7236746e2903187103fff384b6d6060b7a3d
RoshanPanthi11 <roshanpanthi13@gmail.com>
2026-02-10 19:53:11 +0545

    first commit

---
+++ logic_practice/madlibs.py
@@ -0,0 +1,12 @@
+#madlibs game
+adjective1=input("enter an adjective(description):")
+noun1=input("enter a noun(person,place,thing):")
+adjective2=input("enter an adjective(description):")
+verb1=input("enter a verb")
+adjective3=input("enter an adjective(description):")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw a{noun1}")
+print(f"{noun1} was{adjective2} and {verb1}")
+print(f"I was {adjective3}!")
